bcce61f8461569fb2e819349a51786e9734ed84e
Pablo Cesar Altuzar Grajales <134911478+pabloin1@users.noreply.github.com>
2024-11-26 20:37:25 -0600

    Update InitSettings refactor

--- src/main/java/org/example/settings/InitSettings.java
+++ src/main/java/org/example/settings/InitSettings.java
@@ -10,14 +10,19 @@ import org.example.scene.Background;
 
 public class InitSettings {
      public InitSettings(){
+          
          this.createSettings();
+          
          this.objetsSimulation();
+          
      }
 
      private void createSettings(){
+          
          FXGL.getGameScene().getContentRoot().getChildren().add(0, new Background().getBackground());
          Music backgroundMusic = FXGL.getAssetLoader().loadMusic("restaurant.wav");
          FXGL.getAudioPlayer().loopMusic(backgroundMusic);
+          
      }
 
      private void objetsSimulation(){
